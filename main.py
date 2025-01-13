2a60ba8266d389a1ed3160b669bbc0552ecdbabd
Bernmor <bernardo.m.morales@gmail.com>
2025-01-13 16:10:23 -0300

    added player character

--- main.py
+++ main.py
@@ -3,18 +3,30 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
     pygame.init()
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for updates in updatable:
+            updates.update(dt)
         pygame.Surface.fill(screen, (0, 0, 0))
+        for draws in drawable:
+            draws.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
